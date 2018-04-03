54e02e25461a0b1b16dfcae8d5154b6abcff051e
SH <sh@analogic.cz>
2018-04-03 10:17:25 +0200

    README

--- rootfs/etc/cont-init.d/02-lightning.sh
+++ rootfs/etc/cont-init.d/02-lightning.sh
@@ -7,3 +7,9 @@ fi
 if [ ! -d /data/lightning-2 ]; then
     mkdir /data/lightning-2
 fi
+
+ln -s /data/lightning-1 /root/.lightning
+
+if [ ! -f /root/.lightning/lightning-rpc ]; then
+    ln -s /data/lightning-1/rpc /root/.lightning/lightning-rpc
+fi
